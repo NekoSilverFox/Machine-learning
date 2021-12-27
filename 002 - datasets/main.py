138637e44a72eecad419d24282e37b247b60b710
NekoSilverFox <weidufox@gmail.com>
2021-12-27 23:52:48 +0300

    增加 Scikit-learn 的 datasets 的使用

---
+++ 002 - datasets/main.py
@@ -0,0 +1,21 @@
+from sklearn import datasets
+from sklearn.linear_model import LinearRegression
+
+"""从 sklearn 的 datasets 中导入数据"""
+loaded_data = datasets.load_boston()
+
+"""导入属性和标签"""
+data_X = loaded_data.data
+data_y = loaded_data.target
+
+"""导入模型"""
+model = LinearRegression()
+
+"""让他去学习"""
+model.fit(data_X, data_y)
+
+print('用前四个数据预测一下值：')
+print(model.predict(data_X[:4, :]))
+
+print('用前四个数据的真实值：')
+print(data_y[:4])
