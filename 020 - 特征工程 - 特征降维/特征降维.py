1fda4f79e1b7f7280d807776774edd5c54292a56
NekoSilverFox <weidufox@gmail.com>
2022-02-22 18:00:56 +0300

    增加 特征工程 - 特征降维 - 特征选择 - 低方差过滤

---
+++ 020 - 特征工程 - 特征降维/特征降维.py
@@ -0,0 +1,37 @@
+# ------*------ coding: utf-8 ------*------
+# @Time    : 2022/2/22 17:39
+# @Author  : 冰糖雪狸 (NekoSilverfox)
+# @Project : Scikit-learn
+# @File    : 特征降维.py
+# @Software: PyCharm
+# @Github  ：https://github.com/NekoSilverFox
+# -----------------------------------------
+import pandas as pd
+from sklearn import feature_selection
+
+
+def varince():
+    """
+    特征工程 - 特征降维 - 特征选择 - 低方差过滤
+    :return:
+    """
+    # 1. 获取数据
+    data = pd.read_csv('factor_returns.csv')
+    print(data.head(), data.shape)
+
+    # 2. 裁切数据
+    data = data.iloc[:, 1: -2]
+    print(data.head(), data.shape)
+
+
+    # 3. 获取转换器对象
+    transfer = feature_selection.VarianceThreshold(threshold=0.0)
+
+    # 4. 调用 fit_transform
+    data_new = transfer.fit_transform(data)
+    print(data_new, data_new.shape)
+
+
+if __name__ == '__main__':
+    varince()
+    pass
